9a4f73be970424ab0d303bef783b34def93c2a44
Николай Игнатенко <146518175+NikolyaTruLaLaLa@users.noreply.github.com>
2026-09-16 16:08:21 +0300

    fix: fixing names of HistogramBuilder and add doc

--- laba2.Core/Histogram/HistogramBuilder.cs
+++ laba2.Core/Histogram/HistogramBuilder.cs
@@ -2,12 +2,17 @@
 {
     public static class HistogramBuilder
     {
-        public static HistogramData BuildFromBytes(byte[] pixels, int elCount = 256, int binCount = 16, string title = "")
+
+        // pixels - массив произвольной длины с числами/пикселями
+        // maxVal - максимальное значение, которое есть в pixels. Надо чтобы это число делилось на binCOunt. Если что можете взять с запасом.
+        // binCOunt - количество прямоугольников на графике гистограммы. Как видите, каждый прямоугольник отвечает за какой-то промежуток.
+        // title - заголовок гистограммы
+        public static HistogramData BuildFromBytes(byte[] pixels, int maxVal = 256, int binCount = 16, string title = "")
         {
-            if ((elCount % binCount) != 0) throw new ArgumentException($"{nameof(elCount)} should be divided for {nameof(binCount)}");
+            if ((maxVal % binCount) != 0) throw new ArgumentException($"{nameof(maxVal)} should be divided for {nameof(binCount)}");
 
             SortedDictionary<string, int> data = new SortedDictionary<string, int> { };
-            int diap = elCount / binCount;
+            int diap = maxVal / binCount;
             int[] count = new int[binCount];
             for (int i = 0; i < count.Length; ++i)
                 count[i] = 0;
